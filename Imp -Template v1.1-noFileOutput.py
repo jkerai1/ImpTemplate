d6cc63c46ee2b3ccd8a4469f006ece5bbf8639e0
sgsdgsd <55988027+jkerai1@users.noreply.github.com>
2021-05-08 09:38:49 +0100

    Update Imp -Template v1.1-noFileOutput.py
    brought in line with file output version

--- Imp -Template v1.1-noFileOutput.py
+++ Imp -Template v1.1-noFileOutput.py
@@ -1,17 +1,20 @@
+##Random Test IP: 221.241.59.100
+
 import re
-##import datetime
+import sys
 
 ##Random Test IP: 221.241.59.100
 ##this file can be run in an online editor such as https://www.online-python.com/
 
 regex = "^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$"
+punc_filter = re.compile('([/\s]\s*)')
 
 def check(Ip): ##see if IP is valid using Regex
     if(re.search(regex, Ip)):
         pass ##no need to do anything if IP is valid
     else:
-        print("Invalid Ip address")
-        quit()
+        sys.exit("Invalid IP address")
+        
         
 def Printfunc(IP):
     print("\n" + "IP: "  + IP)
@@ -19,90 +22,96 @@ def Printfunc(IP):
 def GetBrowserAndCountry():
     Browser = input("what is browser?   ").lower()
     
-    ######################Clean up shorthand############################### probably a better way to do this using regex#############
+##Clean up shorthand###############################
+
+###Chrome###
     if Browser.__contains__("chr") and not Browser.__contains__("chrome"):
         Browser = (Browser.replace("chr","Chrome"))
+    
+    if Browser.__contains__("cr") and not Browser.__contains__("chrome"): 
+        Browser = (Browser.replace("cr","Chrome"))
         
-    if Browser.__contains__("chrome"):
-        Browser = (Browser.replace("chrome","Chrome"))  # i should probably just use regex to search for "/"
-        
+
+###Firefox####
     if Browser.__contains__("ff") and not Browser.__contains__("ffox"):
         Browser = (Browser.replace("ff","Firefox"))
        
-    if Browser.__contains__("ffox") and not Browser.__contains__("Firefox"):   
+    if Browser.__contains__("ffox") and not Browser.__contains__("firefox"):   
         Browser = (Browser.replace("ffox","Firefox"))
        
-    if Browser.__contains__("firefox"):   
-        Browser = (Browser.replace("firefox","Firefox"))
+        
+###Safari#######        
 
     if Browser.__contains__("saf") and not Browser.__contains__("safari"):
         Browser = (Browser.replace("saf","Safari"))
+
         
-    if Browser.__contains__("safari"):
-        Browser =(Browser.replace("safari","Safari"))
+####Edge#####
         
     if Browser.__contains__("edg") and not Browser.__contains__("edge"):
         Browser = (Browser.replace("edg", "Edge"))
     
-    if Browser.__contains__("edge"):
-        Browser = (Browser.replace("edge", "Edge"))
+###opera###        
+
+    if ("op" in Browser) and not Browser.__contains__("opera"):
+        Browser = (Browser.replace("op", "Opera"))
+###Yandex###
+
+    if "ya" in Browser and not Browser.__contains__("yandex") and not Browser.__contains__("yan"):
+        Browser = (Browser.replace("ya", "Yandex"))
         
-    if Browser.__contains__("opera"):
-        Browser = (Browser.replace("opera", "Opera"))
+    if "yan" in Browser and not ("yandex") in Browser:
+        Browser = (Browser.replace("yan", "Yandex"))
         
-    Country =input ("what is country?   ").upper()
     
-    return (Browser), Country 
+        
+        
+    Browser = punc_filter.split(Browser)
+    Browser = ''.join([i.capitalize() for i in Browser])
+        
+    Country =input ("what is country?   ").upper().replace(" ","") ##convert to upper and remove whitespaces
+    
+    return Browser, Country    
 #####################################################################################################################################################    
 
 while True: ##run infinitely until session timeout
-    IP = (input("IP address: ?"))
+    IP = (input("IP address: ?  ").replace(" ",""))
     check(IP)
-    Error_type = (input("Error_type ?").lower())
+    Error_type = (input("Error_type ? ").lower())
 
-    if Error_type == "ns" or Error_type == "not seen":
+    if Error_type == "ns" or Error_type == "not seen" or Error_type == "n" or Error_type == "not" or Error_type == "no":
         Printfunc(IP)
         print("\n" + "We can confirm that no traffic is seen on the Imperva portal for the IP provided.")
     
-    elif Error_type == "seen" or Error_type == "allowed":
+    elif Error_type == "seen" or Error_type.__contains__("allo") or Error_type == "s":
         Printfunc(IP)
         print("\n" + "We can confirm that traffic for the IP provided is seen on the Imperva portal and is allowed.")
 
-    elif Error_type.__contains__("inv") or Error_type ==  "cookie" or Error_type == "javascript" or Error_type == "js":
+    elif ("inv") in Error_type or ("cook") in Error_type or "java" in Error_type or Error_type == "js":
         Browser, Country = GetBrowserAndCountry()
         Printfunc(IP)
         print("Browser: " + Browser)
         print("Country: "+ Country + "\n")
         print("We can confirm that traffic for the IP provided is currently blocked due to a Javascript or Browser cookie issue.")
     
-    elif Error_type.__contains__("kv") or Error_type.__contains__("vpn") or Error_type.__contains__("host"):
+    elif Error_type.__contains__("kv") or Error_type.__contains__("vp") or Error_type.__contains__("host"):
         Browser, Country = GetBrowserAndCountry()
         Geo = input("what is geo?   ")
+	
+	##Capitalise every first letter in Geo ###
+        GeoTemp = Geo.split()
+        newGeo =""
+        
+        for val in GeoTemp:
+            newGeo += val.capitalize()+  " "     
+	
         Printfunc(IP)
         print("Browser: " + Browser)
         print("Country: "+ Country)
-        print("Geo Org: " + Geo  +"\n")
+        print("Geo Org: " + newGeo  +"\n")
         print("We can confirm the IP provided is currently being blocked due to a VPN or a hosted environment.")
     
     else:
-        print("incorrect option. Error codes are ns, seen, invalid or kvd")
+        print("incorrect option \n Options are: \n\n - NS for not seen \n\n - Seen or s or allowed \n\n - inv/cookie/javascript/js \n\n -Kv/vpn/host ")
         
-    print()    
-    
-    
-    ##Note, it is possible to get python to output to a textfile which could make logging much easier...
-    
-    ##x = datetime.datetime.now()
-    #f =open("Imperva" + x + ".txt", "x") #for a new file. if file already exists then to append use
-    
-    #with open("testfile.txt", "a") as f:
-	    #f.write("I'm an additional line.")
-    ##check if file exists by
-    
-    ##from pathlib import Path
-        
-    #if Path('filename.txt').is_file():
-    
-#        print ("File exist")
-#else:
- #   print ("File not exist")
+    print("\n-------------------------------------------------------------------------------------------------------------------------")    
